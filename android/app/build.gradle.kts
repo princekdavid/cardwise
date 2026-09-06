69096872f4c6cc3cf084852bb1ea169e28f9a12e
Prince Kumar Niraj <princekumarniraj21@gmail.com>
2026-09-06 09:30:25 +0530

    fix: apply Compose BOM to Android test dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test:2.0.21")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
 
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.01.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test:runner:1.6.2")
