7f56e4afa34c0da35bca0015f9b97c5ad4692f73
Prince Kumar Niraj <princekumarniraj21@gmail.com>
2026-09-08 05:01:13 +0530

    build: configure Room schema plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("org.jetbrains.kotlin.kapt") version "2.0.21" apply false
+    id("androidx.room") version "2.8.4" apply false
 }
